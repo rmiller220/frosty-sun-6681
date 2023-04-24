9257cd41c72e6afb97045812c7fcfeae142d704b
Reilly Miller <reillyjmiller31@gmail.com>
2023-04-24 09:44:15 -0600

    Complete user story1

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,7 @@
+class DoctorsController < ApplicationController
+  def show
+    @doctor = Doctor.find(params[:id])
+    @doctor_hospital = @doctor.hospital_name
+    @patients = @doctor.patients
+  end
+end
