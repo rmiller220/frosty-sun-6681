e8ed69a62ce5c92e1e9b60c1e10b4ab43617b79a
Reilly Miller <reillyjmiller31@gmail.com>
2023-04-24 10:35:56 -0600

    complete story2, delete patient from doctor workload

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,9 @@
 class PatientsController < ApplicationController
+  
+  def destroy
+    @doctor = Doctor.find(params[:doctor_id])
+    @patient = Patient.find(params[:id])
+    @doctor.patients.destroy(@patient)
+    redirect_to doctor_path(@doctor)
+  end
 end
